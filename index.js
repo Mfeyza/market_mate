a8d9864035dd71b38aa67d817ee59e6e34757714
M. Feyza Yıldırım <144602340+Mfeyza@users.noreply.github.com>
2024-05-21 22:04:28 +0300

    feat:live

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 
 
 require('dotenv').config()
-const HOST = process.env?.HOST || '127.0.0.1'
+
 const PORT = process.env?.PORT || 8000
 
 
@@ -52,8 +52,7 @@ app.use(require('./src/routes'))
 
 app.use(require('./src/middlewares/errorHandler'))
 
-
-app.listen(PORT, HOST, () => console.log(`http://${HOST}:${PORT}`))
+app.listen(PORT,()=> console.log(`http://:${PORT}`))
 
 /* ------------------------------------------------------- */
 
